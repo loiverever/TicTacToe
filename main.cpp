2afd4167b71342db7306f240224b49082dc30416
i-dontgiveafox <jamesasperilla@gmail.com>
2024-06-23 18:26:23 +0800

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,45 +1,89 @@
 #include <iostream>
 #include <string>
+#include <chrono>
+#include <thread>
 using namespace std;
 
 int main() {
-    //Contributions:
-    //James Welcome (ASCII ART)
+
+    // For console output delay
+    using namespace std::this_thread;     // sleep_for, sleep_until
+    using namespace std::chrono_literals; // ns, us, ms, s, h, etc.
+    using std::chrono::system_clock;
+
+    // Contributions:
+    // James Welcome (ASCII ART)
+    cout << " __     __     ______     __         ______     ______     __    __     ______    \n";
+    cout << "/\\ \\  _ \\ \\   /\\  ___\\   /\\ \\       /\\  ___\\   /\\  __ \\   /\\ \"-./  \\   /\\  ___\\   \n";
+    cout << "\\ \\ \\/ \".\\ \\  \\ \\  __\\   \\ \\ \\____  \\ \\ \\____  \\ \\ \\/\\ \\  \\ \\ \\-./\\ \\  \\ \\  __\\   \n";
+    cout << " \\ \\__/\".~\\_\\  \\ \\_____\\  \\ \\_____\\  \\ \\_____\\  \\ \\_____\\  \\ \\_\\ \\ \\_\\  \\ \\_____\\ \n";
+    cout << "  \\/_/   \\/_/   \\/_____/   \\/_____/   \\/_____/   \\/_____/   \\/_/  \\/_/   \\/_____/ \n";
+
+    cout << "" << endl;
+
+    cout << "\t\t\t\t ______   ______    \n";
+    cout << "\t\t\t\t/\\__  _\\ /\\  __ \\   \n";
+    cout << "\t\t\t\t\\/_/\\ \\/ \\ \\ \\/\\ \\  \n";
+    cout << "\t\t\t\t   \\ \\_\\  \\ \\_____\\ \n";
+    cout << "\t\t\t\t    \\/_/   \\/_____/ \n";
+
+    cout << "" << endl;
+
+    cout << " ______   __     ______     ______   ______     ______     ______   ______     ______    \n";
+    cout << "/\\__  _\\ /\\ \\   /\\  ___\\   /\\__  _\\ /\\  __ \\   /\\  ___\\   /\\__  _\\ /\\  __ \\   /\\  ___\\   \n";
+    cout << "\\/_/\\ \\/ \\ \\ \\  \\ \\ \\____  \\/_/\\ \\/ \\ \\  __ \\  \\ \\ \\____  \\/_/\\ \\/ \\ \\ \\/\\ \\  \\ \\  __\\   \n";
+    cout << "   \\ \\_\\  \\ \\_\\  \\ \\_____\\    \\ \\_\\  \\ \\_\\ \\_\\  \\ \\_____\\    \\ \\_\\  \\ \\_____\\  \\ \\_____\\ \n";
+    cout << "    \\/_/   \\/_/   \\/_____/     \\/_/   \\/_/\\/_/   \\/_____/     \\/_/   \\/_____/   \\/_____/ \n";
+    
+    cout << "" << endl;
+    cout << "" << endl;
+
+    sleep_for(3s);
+    sleep_until(system_clock::now() + 1s);
+
+    // Giuliani Instructions of the game
+
+    // Shanley Enter the player's name, adventure message, winner message, tie message
+
+    // Meg While loop
+
+    // Kate Score Tracking
 
     //Giuliani Instructions of the game
+    cout << "-------------------------------------------------------------------------------------------------" << endl;
+    cout << "" << endl;
+    cout << "\t\t\t\t\tInstructions:" << endl << endl;
+    cout << "-------------------------------------------------------------------------------------------------" << endl;
 
-    //Shanley Enter the player's name, adventure message, winner message, tie message
+    cout << "" << endl;
+    cout << "• The game is played on a 3x3 grid." << endl;
+    cout << "• Player 1 is 'X' and Player 2 is 'O'." << endl;
+    cout << "• Players take turns placing their marks ('X' or 'O') in empty squares." << endl;
+    cout << "• Enter the row and column numbers (0, 1, or 2) separated by a space to place your mark." << endl;
+    cout << "• Rows and columns are numbered from 0 to 2, from top to bottom and left to right." << endl;
+    cout << "• For example, to place your mark in the top-left corner, enter '0 0'." << endl;
+    cout << "• Ensure your inputs are valid numbers between 0 and 2." << endl;
+    cout << "• The first player to get 3 of their marks in a row (horizontally, vertically, or diagonally) wins." << endl;
+    cout << "• If all 9 squares are full and no player has 3 in a row, the game is a tie." << endl << endl;
+    cout << "-------------------------------------------------------------------------------------------------" << endl;
+    
+    cout << "" << endl;
 
-    //Meg While loop
-
-    //Kate Score Tracking
-
-
-
-    //Giuliani Instructions of the game
-    cout << "Instructions:" << endl << endl;
-    cout << "The game is played on a 3x3 grid." << endl;
-    cout << "Player 1 is 'X' and Player 2 is 'O'." << endl;
-    cout << "Players take turns placing their marks ('X' or 'O') in empty squares." << endl;
-    cout << "Enter the row and column numbers (0, 1, or 2) separated by a space to place your mark." << endl;
-    cout << "Rows and columns are numbered from 0 to 2, from top to bottom and left to right." << endl;
-    cout << "For example, to place your mark in the top-left corner, enter '0 0'." << endl;
-    cout << "Ensure your inputs are valid numbers between 0 and 2." << endl;
-    cout << "The first player to get 3 of their marks in a row (horizontally, vertically, or diagonally) wins." << endl;
-    cout << "If all 9 squares are full and no player has 3 in a row, the game is a tie." << endl << endl;
-
-
-
-
-    //store players score
+    // Store players score
     int scorePlayer1 = 0, scorePlayer2 = 0;
 
 
-    //Shanley Enter the player's name, adventure message, winner message, tie message
+    // Shanley Enter the player's name, adventure message, winner message, tie message
     // Variables to store player names
     string player1, player2;
 
+    cout << " ______   __         ______     __  __     ______     ______        ______     __   __     ______   ______     __  __    \n";
+    cout << "/\\  == \\ /\\ \\       /\\  __ \\   /\\ \\_\\ \\   /\\  ___\\   /\\  == \\      /\\  ___\\   /\\ \"-.\\ \\   /\\__  _\\ /\\  == \\   /\\ \\_\\ \\   \n";
+    cout << "\\ \\  _-/ \\ \\ \\____  \\ \\  __ \\  \\ \\____ \\  \\ \\  __\\   \\ \\  __<      \\ \\  __\\   \\ \\ \\-.  \\  \\/_/\\ \\/ \\ \\  __<   \\ \\____ \\  \n";
+    cout << " \\ \\_\\    \\ \\_____\\  \\ \\_\\ \\_\\  \\/\\_____\\  \\ \\_____\\  \\ \\_\\ \\_\\     \\ \\_____\\  \\ \\_\\\"\\_\\    \\ \\_\\  \\ \\_\\ \\_\\  \\/\\_____\\ \n";
+    cout << "  \\/_/     \\/_____/   \\/_/\\/_/   \\/_____/   \\/_____/   \\/_/ /_/      \\/_____/   \\/_/ \\/_/     \\/_/   \\/_/ /_/   \\/_____/ \n";
 
+    cout << "" << endl;
 
     // Input name of the players
     cout << "Enter name of Player 1 (X): ";
@@ -47,11 +91,23 @@ int main() {
     cout << "Enter name of Player 2 (O): ";
     cin >> player2;
 
+    cout << "" << endl;
+
+    cout << " ______     ______     __    __     ______        ______     ______   ______     ______     ______  \n";
+    cout << "/\\  ___\\   /\\  __ \\   /\\ \"-./  \\   /\\  ___\\      /\\  ___\\   /\\__  _\\ /\\  __ \\   /\\  == \\   /\\__  _\\ \n";
+    cout << "\\ \\ \\__ \\  \\ \\  __ \\  \\ \\ \\-./\\ \\  \\ \\  __\\      \\ \\___  \\  \\/_/\\ \\/ \\ \\  __ \\  \\ \\  __<   \\/_/\\ \\/ \n";
+    cout << " \\ \\_____\\  \\ \\_\\ \\_\\  \\ \\_\\ \\ \\_\\  \\ \\_____\\     \\/\\_____\\    \\ \\_\\  \\ \\_\\ \\_\\  \\ \\_\\ \\_\\    \\ \\_\\ \n";
+    cout << "  \\/_____/   \\/_/\\/_/   \\/_/  \\/_/   \\/_____/      \\/_____/     \\/_/   \\/_/\\/_/   \\/_/ /_/     \\/_/ \n";
+
+    cout << "" << endl;
+    
     // Good luck message before the game starts
     cout << endl << "Good luck, " << player1 << " and " << player2 << "!" << endl << endl;
 
+    sleep_for(3s);
+    sleep_until(system_clock::now() + 1s);
 
-    //Meg While Loop
+    // Meg While Loop
     bool playAgain = true;
 
     while (playAgain) {
@@ -98,8 +154,12 @@ int main() {
                     cout << "Tile is full, try again." << endl;
                 }
                 else {
+                    sleep_for(2s);
+                    sleep_until(system_clock::now() + 1s);
+
                     break;
                 }
+
                 // Reset values
                 row = -1;
                 column = -1;
@@ -135,7 +195,7 @@ int main() {
                 winner = board[0][2];
             }
 
-            //Kate Score Tracking
+            // Kate Score Tracking
             // If there is a winner, update scores
             if (winner == playerX) {
                 scorePlayer1++;
@@ -146,22 +206,46 @@ int main() {
 
         // Game result
         if (winner != ' ') {
+            cout << "" << endl;
+            cout << " __     __     __     __   __     __   __     ______     ______    \n";
+            cout << "/\\ \\  _ \\ \\   /\\ \\   /\\ \"-.\\ \\   /\\ \"-.\\ \\   /\\  ___\\   /\\  == \\   \n";
+            cout << "\\ \\ \\/ \".\\ \\  \\ \\ \\  \\ \\ \\-.  \\  \\ \\ \\-.  \\  \\ \\  __\\   \\ \\  __<   \n";
+            cout << " \\ \\__/\".~\\_\\  \\ \\_\\  \\ \\_\\\"\\_\\  \\ \\_\\\"\\_\\  \\ \\_____\\  \\ \\_\\ \\_\\ \n";
+            cout << "  \\/_/   \\/_/   \\/_/   \\/_/ \\/_/   \\/_/ \\/_/   \\/_____/   \\/_/ /_/ \n";
+            cout << "" << endl;
             cout << endl <<"Player " << winner << " (" << (winner == playerX ? player1 : player2) << ") is the winner!" << endl;
             cout << endl;
         }
         else {
-            cout << endl << "It's a tie! Well played, " << player1 << " and " << player2 << "!" << endl;
+            cout << "" << endl;
+            cout << " _____     ______     ______     __     __    \n";
+            cout << "/\\  __-.  /\\  == \\   /\\  __ \\   /\\ \\  _ \\ \\   \n";
+            cout << "\\ \\ \\/\\ \\ \\ \\  __<   \\ \\  __ \\  \\ \\ \\/ \".\\ \\  \n";
+            cout << " \\ \\____-  \\ \\_\\ \\_\\  \\ \\_\\ \\_\\  \\ \\__/\".~\\_\\ \n";
+            cout << "  \\/____/   \\/_/ /_/   \\/_/\\/_/   \\/_/   \\/_/ \n";
+            cout << "" << endl;
+            cout << endl << "It's a draw! Well played, " << player1 << " and " << player2 << "!" << endl;
             cout << endl;
         }
 
         // Display scores
-        cout << "Scores:" << endl;
+        cout << "-------------------------------------------------------------------------------------------------" << endl;
+        cout << "   ___     ___     ___     ___     ___     ___   \n";
+        cout << "  / __|   / __|   / _ \\   | _ \\   | __|   / __|  \n";
+        cout << "  \\__ \\  | (__   | (_) |  |   /   | _|    \\__ \\  \n";
+        cout << "  |___/   \\___|   \\___/   |_|_\\   |___|   |___/  \n";
+        cout << "_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"| \n";
+        cout << "\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-' \n";
+
+        cout << "" << endl;
         cout << player1 << ": " << scorePlayer1 << endl;
         cout << player2 << ": " << scorePlayer2 << endl;
         cout << endl;
+        cout << "-------------------------------------------------------------------------------------------------" << endl;
 
         // Ask if they want to play again
         char playAgainChoice;
+        cout << "" << endl;
         cout << "Do you want to play again? (y/n): ";
 
         cin >> playAgainChoice;
